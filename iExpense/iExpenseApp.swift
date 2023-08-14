ceb1e3fee7bf21b08a58bf6c497db95696fc68eb
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-08-14 17:57:17 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Monty Harper on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
